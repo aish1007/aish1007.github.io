2c1863853e73ff37422f83aa277bffdba7c3e45a
aish1007 <aishwarya.narayanan025@gmail.com>
2020-07-12 22:55:28 -0700

    modified directory structure

--- content-service/contentful-client.js
+++
@@ -1,8 +0,0 @@
-var contentful = require("contentful");
-
-var client = contentful.createClient({
-  accessToken: "DS7KfMVF1VLXe8ynPoyY6QH5EfoOzqK2Gou1F_HTc9g",
-  space: "25l2wnuwupd4",
-});
-
-exports.client = client;
